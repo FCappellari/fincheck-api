ef96912aa9bd3a760fa34e8c37309914af7c0aaf
Fabrício Cappellari <fabricio.cappellari@live.com>
2025-01-23 19:18:41 -0300

    refactor: reorganize user authentication logic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SigninDto } from './dto/signin';
+import { SignupDto } from './dto/signup';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('signup')
+  create(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+
   @Post('signin')
-  authenticate(@Body() auuthenticateDTO: AuthenticateDto) {
-    return this.authService.authenticate(auuthenticateDTO);
+  authenticate(@Body() signinDTO: SigninDto) {
+    return this.authService.signin(signinDTO);
   }
 }
