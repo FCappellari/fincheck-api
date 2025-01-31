01914fa3d0d54c276fad70748fc6cacfae8dc307
Fabrício Cappellari <fabricio.cappellari@live.com>
2025-01-30 23:57:07 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards();
+  app.enableCors({ origin: '*' });
 
   await app.listen(process.env.PORT ?? 3001);
 }
